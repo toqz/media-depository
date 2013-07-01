d93a867a04fde474e00ab4555d8dd3fbb82d5b83
RaymundT@ny.rga.com <raymund.tuquero@rga.com>
2013-07-01 18:31:55 +0800

    initial commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('MediaDepositoryApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boiler',
+      'Angul',
+      'Karms'
+    ];
+  });
